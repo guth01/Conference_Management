e811420afd45d1916c3f14ff01a0f287dc5a5677
Karthikeyan KS <86113017+KarthikeyanKS00747@users.noreply.github.com>
2024-03-29 13:04:09 +0530

    Update Organiser.h

---
+++ Organiser.h
@@ -0,0 +1,30 @@
+#ifndef ORGANISER_H
+#define ORGANISER_H
+
+#include <string>
+#include "User.h"
+
+class Organiser : public User 
+{
+private:
+    std::string organisationName;
+    std::string organiserTitle;
+
+public:
+    // Constructor
+    Organiser(std::string name, short int age, std::string regNO, std::string gender, 
+              std::string username, std::string password, std::string email, 
+              std::string orgName, std::string title);
+
+    // Getters and Setters
+    void setOrganisationName(std::string orgName);
+    std::string getOrganisationName() const;
+
+    void setOrganiserTitle(std::string title);
+    std::string getOrganiserTitle() const;
+
+    // Display organiser information
+    void displayOrganiserInfo() const;
+};
+
+#endif // ORGANISER_H
